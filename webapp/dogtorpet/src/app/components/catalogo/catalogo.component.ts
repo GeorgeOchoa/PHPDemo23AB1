046a7faeb179efa0d2ecc7eda0ea5dd2b0575ba1
atellezf <alejandro@correo.com>
2023-01-14 10:10:10 -0600

    Código de la OpenClass S2

--- webapp/dogtorpet/src/app/components/catalogo/catalogo.component.ts
+++ webapp/dogtorpet/src/app/components/catalogo/catalogo.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Mascota } from 'src/app/models/mascota';
+import { mascotas } from '../../services/datos-prueba';
 
 @Component({
   selector: 'app-catalogo',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./catalogo.component.css']
 })
 export class CatalogoComponent {
+
+  mascotas!:Mascota[];
+
+  constructor() {
+    this.mascotas = mascotas;
+  }
 
 }
