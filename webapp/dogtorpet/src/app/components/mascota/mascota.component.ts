51455e4f0933f5ea9b3142f033edcc94b3a23d05
atellezf <alejandro@correo.com>
2023-01-28 09:34:08 -0600

    Código de la OpenClass S4

--- webapp/dogtorpet/src/app/components/mascota/mascota.component.ts
+++ webapp/dogtorpet/src/app/components/mascota/mascota.component.ts
@@ -1,5 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Mascota } from 'src/app/models/mascota';
+import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { EliminarComponent } from '../eliminar/eliminar.component';
 
 @Component({
   selector: 'app-mascota',
@@ -11,14 +14,22 @@ export class MascotaComponent {
   @Input()
   mascota!: Mascota;
 
+  @Output()
+  emisor = new EventEmitter<Mascota>();
+
+  constructor( private router:Router, private modalSvc:NgbModal) { }
+
   public editar(): void {
     // TODO: Implementar lógica para editar una mascota
     console.log("Editar registro de mascota");
   }
 
   public confirmarEliminar( evt:Event ): void {
-    // TODO: Implementar lógica para confirmación
-    console.log("Confirmar eliminación de mascota");
+    const dialogo = this.modalSvc.open(EliminarComponent, { centered:true });
+    dialogo.componentInstance.mensaje = `¿Estás seguro de eliminar el registro de ${this.mascota.nombre}?`;
+    dialogo.result.then( opcion => {
+      if( opcion ) this.emisor.emit(this.mascota);
+    } );
     evt.stopPropagation();
   }
 
